c11a1507bfec6c1e1e5b21ef65f661be30360630
Pira4791 <Artur2111094@mail.ru>
2021-05-14 21:21:26 +0300

    task 5

--- src/Task5.java
+++ src/Task5.java
@@ -8,13 +8,13 @@ public class Task5 {
         if (time > 0 && time <= 6) {
             System.out.println("Good Night");
         }
-        if (time > 6 && time <= 13) {
+        else if (time > 6 && time <= 13) {
             System.out.println("Good Morning");
         }
-        if (time > 13 && time <= 17) {
+        else if (time > 13 && time <= 17) {
             System.out.println("Good Day");
         }
-        if (time > 17 && time <= 24) {
+        else if (time > 17 && time <= 24) {
             System.out.println("Good Evening");
         } else {
             System.out.println("Not a time");
